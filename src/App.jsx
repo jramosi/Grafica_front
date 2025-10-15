d2d06148f694faa1ec9a51c6a7d276131291c47a
Jhonatan Ramos <jkevinramos59@gmail.com>
2025-10-14 21:36:21 -0400

    los tres graficos

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,21 @@ import TelemetryPresion from './TelemetryPresion'
 import TelemetryHume from './TelemetryHume'
 
 const App = () => {
-  const [opcion, setOpcion] = useState('');
-
-  const handleChange = (event) => {
-    setOpcion(event.target.value);
-  }; 
-
   return (
-    <div>
-      <h2>Selecciona una opción:</h2>
-      <select value={opcion} onChange={handleChange}>
-        <option value="">-- Elige --</option>
-        <option value="THB-02">THB-02</option>
-        <option value="THB-03">THB-03</option>
-        <option value="THB-04">THB-04</option>
-      </select>
-      {opcion && <p>Seleccionaste: <strong>{opcion}</strong></p>}
-      <div>
-        <div>
+    
+    <>
+      <div className='app-container'>
+        <div className='card'>
           <TelemetryTempe th={opcion}/>
         </div>
-        <div>
+        <div className='card'>
           <TelemetryPresion th={opcion}/>
         </div>
-        <div>
+        <div className='card'>
           <TelemetryHume th={opcion} />
         </div>
       </div>
-    </div>
+    </> 
   );
 };
 
