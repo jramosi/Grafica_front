8f95c4a096f36478ec65ab9e671a2594cf4e8ce0
Jhonatan Ramos <jkevinramos59@gmail.com>
2025-10-14 21:15:26 -0400

    los tres graficos

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import './index.css'
+import './index.css'
 
 //import TelemetryClient from './TelemetryClient'
 import TelemetryTempe from './TelemetryTempe'
